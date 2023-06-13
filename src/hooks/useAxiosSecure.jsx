76374f658efdab5af3b2970cd1573d811ffd22de
skmajumder <skmajumder.cse@gmail.com>
2023-06-13 15:40:41 +0600

    Axios Instance and Interceptors

---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,50 @@
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useEffect } from "react";
+
+const useAxiosSecure = () => {
+  const { logOut } = useAuth();
+  const navigate = useNavigate();
+
+  // * Axios instance with base URL
+  const axiosSecure = axios.create({
+    baseURL: "http://localhost:3000",
+  });
+
+  const setupInterceptors = () => {
+    // * Request Interceptors
+    axiosSecure.interceptors.request.use((config) => {
+      const token = localStorage.getItem("access_token");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    });
+
+    // * Response interceptor
+    axiosSecure.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      async (error) => {
+        if (
+          error.response &&
+          (error.response.status === 401 || error.response.status === 403)
+        ) {
+          await logOut();
+          navigate("/login");
+        }
+        return Promise.reject(error);
+      }
+    );
+  };
+
+  useEffect(() => {
+    setupInterceptors();
+  }, [logOut, navigate, axiosSecure]);
+
+  return axiosSecure;
+};
+
+export default useAxiosSecure;
--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "./useAuth";
+import useAxiosSecure from "./useAxiosSecure";
 
 const useCart = () => {
   const { user, loading } = useAuth();
-  const token = localStorage.getItem("access_token");
+  const axiosSecure = useAxiosSecure();
 
   const {
     isLoading,
@@ -13,17 +14,11 @@ const useCart = () => {
     queryKey: ["carts", user?.email],
     enabled: !loading,
     queryFn: async () => {
-      const response = await fetch(
-        `http://localhost:3000/carts?email=${user?.email}`,
-        {
-          headers: {
-            authorization: `Bearer ${token}`,
-          },
-        }
-      );
-      return response.json();
+      const response = await axiosSecure(`/carts?email=${user?.email}`);
+      return response.data;
     },
   });
+
   return { carts, isLoading, refetch };
 };
 
