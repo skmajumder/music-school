797fc89ca8d97523027d63330fe7357d9af2cb6e
skmajumder <skmajumder.cse@gmail.com>
2023-06-14 13:48:19 +0600

    class feedback

--- src/pages/Dashboard/ManageClasses/ManageClasses.jsx
+++ src/pages/Dashboard/ManageClasses/ManageClasses.jsx
@@ -3,11 +3,22 @@ import PageTitle from "../../../components/PageTitle/PageTitle";
 import useClass from "../../../hooks/useClass";
 import Swal from "sweetalert2";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useAuth from "../../../hooks/useAuth";
+import { useForm } from "react-hook-form";
 
 const ManageClasses = () => {
+  const { user } = useAuth();
   const [searchTerm, setSearchTerm] = useState("");
   const { classes, courseRefetch } = useClass();
   const { axiosSecure } = useAxiosSecure();
+  const [modalClass, setModalClass] = useState({});
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
@@ -24,6 +35,7 @@ const ManageClasses = () => {
     axiosSecure
       .patch(`/classes/status/${courseID}`, {
         status: status,
+        email: user.email,
       })
       .then((res) => {
         if (res.data.modifiedCount > 0) {
@@ -34,12 +46,92 @@ const ManageClasses = () => {
   };
 
   const handleFeedback = (course) => {
-    console.log(course);
+    setModalClass(course);
+    window.my_modal_2.showModal();
+  };
+
+  const onSubmit = (data) => {
+    axiosSecure
+      .patch(`/classes/feedback/${modalClass?._id}`, {
+        feedback: data.feedback,
+        email: user.email,
+      })
+      .then((res) => {
+        if (res.data.modifiedCount > 0) {
+          courseRefetch();
+          reset();
+          Swal.fire(
+            "Done!",
+            `${modalClass.className} class Feedback done`,
+            "success"
+          );
+        }
+      });
+    closeModal();
+  };
+
+  // Close the modal
+  const closeModal = () => {
+    window.my_modal_2.close();
   };
 
   return (
     <>
       <PageTitle title={"Manage Classes"} />
+      <dialog id="my_modal_2" className="modal">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          method="dialog"
+          className="modal-box"
+        >
+          <p className="py-4">Press ESC key or click outside to close</p>
+          <div className="space-y-2 mb-2">
+            <p>Class: {modalClass.className}</p>
+            {modalClass.status === "approved" && (
+              <>
+                Status:{" "}
+                <p className="badge badge-success uppercase text-white">
+                  {modalClass.status}
+                </p>
+              </>
+            )}
+            {modalClass.status === "denied" && (
+              <>
+                Status:{" "}
+                <p className="badge badge-error uppercase text-white">
+                  {modalClass.status}
+                </p>
+              </>
+            )}
+          </div>
+          <div className="flex flex-col space-y-2">
+            <label htmlFor="feedback" className="text-sm font-bold">
+              Feedback
+            </label>
+            <textarea
+              {...register("feedback", { required: true })}
+              id="feedback"
+              rows="5"
+              className="border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-1 focus:ring-blue-500"
+              placeholder={`Please write the ${modalClass.status} reason`}
+            ></textarea>
+            {errors.feedback && (
+              <p className="text-red-500 text-sm">This field is required</p>
+            )}
+          </div>
+          <div className="flex justify-end mt-2">
+            <button
+              type="submit"
+              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
       <section className="flex-grow">
         <div className="container mx-auto p-4">
           <h3 className="text-center text-3xl font-medium mb-7">
@@ -123,7 +215,7 @@ const ManageClasses = () => {
                             Deny
                           </button>
                           <button
-                            onClick={() => handleFeedback(item?._id)}
+                            onClick={() => handleFeedback(item)}
                             data-tip="Give Feedback"
                             className="btn btn-sm capitalize tooltip tooltip-left text-blue-500 text-[12px]"
                           >
